f8ffeeadd8f7dcbc0f85e75c96f4e93c94834eb4
neda-ara <neda.ara9@gmail.com>
2022-07-23 06:18:51 +0530

    User dropdown

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
-import { BsFillGridFill } from "react-icons/bs";
+import {
+  BsFillGridFill,
+  BsMusicNoteList,
+  BsFillGearFill,
+  BsBoxArrowRight,
+  BsPersonCircle,
+} from "react-icons/bs";
 import { BiChevronDownCircle } from "react-icons/bi";
 import { ImSearch } from "react-icons/im";
 import { MdAccountCircle } from "react-icons/md";
 
 const Navbar = () => {
+  const [userDropdownOpen, setUserDropdownOpen] = useState(false);
+
   return (
     <div className="nav__wrapper" id="navbar">
       <div className="search__container center">
@@ -24,7 +32,34 @@ const Navbar = () => {
             <MdAccountCircle className="user-icon" />
           </div>
           <p className="user-name">Moon9</p>
-          <BiChevronDownCircle className="down-arrow-icon" />
+          <BiChevronDownCircle
+            className="down-arrow-icon"
+            onClick={() => {
+              setUserDropdownOpen(!userDropdownOpen);
+            }}
+            style={{ transform: userDropdownOpen ? "rotate(180deg)" : "" }}
+          />
+          <div
+            className="user-menu__dropdown"
+            style={{ display: userDropdownOpen ? "block" : "none" }}
+          >
+            <div className="dropdown__item">
+              <BsPersonCircle className="menu-icons" />
+              <p className="menu-item-text">My Profile</p>
+            </div>
+            <div className="dropdown__item">
+              <BsMusicNoteList className="menu-icons" />
+              <p className="menu-item-text">Library</p>
+            </div>
+            <div className="dropdown__item">
+              <BsFillGearFill className="menu-icons" />
+              <p className="menu-item-text">Settings</p>
+            </div>
+            <div className="dropdown__item">
+              <BsBoxArrowRight className="menu-icons" />
+              <p className="menu-item-text logout">Log out</p>
+            </div>
+          </div>
         </div>
         <div className="actions__container">
           <label>
